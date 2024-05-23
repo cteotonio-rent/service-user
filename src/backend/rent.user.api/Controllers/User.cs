c14f9518b3fc0141cb20beb38bb05810963bdf98
cleteot <54756732+cleteot@users.noreply.github.com>
2024-05-23 20:15:55 -0300

    feat: adding endpoint to register an user

---
+++ src/backend/rent.user.api/Controllers/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using rent.user.communication.Requests;
+using rent.user.communication.Responses;
+
+namespace rent.user.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class User : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson),statusCode:StatusCodes.Status201Created)]
+        public IActionResult Post([FromBody] ResquestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/shared/rent.user.communication/Requests/ResquestRegisterUserJson.cs
@@ -0,0 +1,9 @@
+﻿namespace rent.user.communication.Requests
+{
+    public class ResquestRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
