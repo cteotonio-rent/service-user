ab54275391ef35b368414418f3e199de9fd36242
cleteot <54756732+cleteot@users.noreply.github.com>
2024-05-26 10:43:17 -0300

    feat: adding sonar-cloud

---
+++ src/tests/CommomTestUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,15 @@
+﻿using rent.user.application.Services.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommomTestUtilities.Cryptography
+{
+    public class PasswordEncripterBuilder
+    {
+        public static PasswordEncripter Build() => new PasswordEncripter("abc123");
+
+    }
+}
---
+++ src/tests/CommomTestUtilities/Mapper/MapperBuilder.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using rent.user.application.Services.AutoMapper;
+
+namespace CommomTestUtilities.Mapper
+{
+    public class MapperBuilder
+    {
+        public static IMapper Build()
+        {
+            return new MapperConfiguration(option =>
+            {
+                option.AddProfile(new AutoMapping());
+            }).CreateMapper();
+        }
+    }
+}
